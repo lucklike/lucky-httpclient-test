db4e2532f0c6c6a9dd2805155e5196a2de42c8fe
fukang1163 <fukang1163@cairenhui.com>
2024-03-21 17:48:29 +0800

    20240321 糟糕的一天，分开时迟早的事情

--- http-java/src/main/java/io/github/lucklike/testcase/api/TranslationApi.java
+++ http-java/src/main/java/io/github/lucklike/testcase/api/TranslationApi.java
@@ -1,13 +1,13 @@
 package io.github.lucklike.testcase.api;
 
 import cn.hutool.crypto.digest.DigestUtil;
-import com.luckyframework.common.EncryptionUtils;
 import com.luckyframework.common.NanoIdUtils;
 import com.luckyframework.common.StringUtils;
 import com.luckyframework.httpclient.proxy.annotations.DomainName;
 import com.luckyframework.httpclient.proxy.annotations.Post;
 import com.luckyframework.httpclient.proxy.annotations.SpELVar;
 import com.luckyframework.httpclient.proxy.annotations.SpElSelect;
+import com.luckyframework.httpclient.proxy.annotations.StaticHeader;
 import com.luckyframework.httpclient.proxy.annotations.StaticQuery;
 import com.luckyframework.httpclient.proxy.context.Context;
 import com.luckyframework.serializable.SerializationSchemeFactory;
@@ -22,12 +22,14 @@ import io.github.lucklike.util.SM4Util;
 @SpELVar({
         "appid=#{$this$.SM4('a94edf938ecd3a2a8ca013bd800b52ad')}",
         "privatekey=#{$this$.SM4('b2482869665723323117ea6d00de9818833788eecce62cbfe88f6baeb23eb08ef59031af1aece287fe54e8b6c383eb3f')}",
-        "nonce_str=#{$this$.nanoId()}"
+        "nonce_str=#{$this$.nanoId()}",
+        "abc=#{nonce_str}-#{appid}-#{privatekey}"
 })
 @DomainName("https://www.fanyigou.com")
 public interface TranslationApi {
 
     @SpElSelect("#{$this$.fromJson($body$).data.transResult}")
+    @StaticHeader("abc=#{abc}")
     @StaticQuery({"from=zh", "to=en", "nonce_str=#{nonce_str}", "token=#{$this$.getToken($mc$, text)}"})
     @Post("/TranslateApi/api/trans?appid=#{appid}")
     String trans(String text);
