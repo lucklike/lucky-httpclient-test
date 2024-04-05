0aca02bb921ccdc66532e291885e5576f34ddbeb
fk7075 <1814375626@qq.com>
2024-04-05 12:48:41 +0800

    20240405 Gzip案例编写

--- http-java/src/main/java/io/github/lucklike/testcase/api/MyApi.java
+++ http-java/src/main/java/io/github/lucklike/testcase/api/MyApi.java
@@ -1,5 +1,6 @@
 package io.github.lucklike.testcase.api;
 
+import com.luckyframework.httpclient.proxy.annotations.GzipCompress;
 import com.luckyframework.httpclient.proxy.annotations.HttpRequest;
 import com.luckyframework.httpclient.proxy.annotations.StaticHeader;
 
@@ -8,6 +9,7 @@ import static com.luckyframework.httpclient.core.RequestMethod.GET;
 
 /**
  * 请求百度首页
+ *
  * @author fukang
  * @version 1.0.0
  * @date 2024/2/24 15:45
@@ -18,7 +20,8 @@ public interface MyApi {
     	GET http://www.baidu.com
 	    User-Agent: Lucky-HttpClient/2.1.0 (Java/1.8.0_301)
      */
-    @HttpRequest(url = "http://www.baidu.com", method = GET)
+    @GzipCompress
+    @HttpRequest(url = "http://www.baidu.com/", method = GET)
     String index();
 
     /*
